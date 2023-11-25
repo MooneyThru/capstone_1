cd83d7150026a7b9b1ac50566e82e22ffdfc2577
MooneyThru <151181075+MooneyThru@users.noreply.github.com>
2023-11-25 22:19:39 +0900

    Add files via upload

--- master_control.py
+++ master_control.py
@@ -5,7 +5,7 @@ import numpy as np
 import json
 
 # port = '/dev/ttyACM0' # for Linux
-port = 'COM7'         # for Windows
+port = 'COM3'         # for Windows
 robo = serial.Serial(port, 9600)  # 'COM포트'는 아두이노가 연결된 포트로 수정
 time.sleep(2)  # 시리얼 통신 시작 후 잠시 대기
 # 파일에서 첫 번째 단어 가져오기
@@ -43,42 +43,118 @@ def retrieve_cart_items(file_path):
     except FileNotFoundError:
         print("Cart items file not found.")
         return []
-
+def get_last_object(item):
+    try:
+        with open('detected_objects.json', 'r') as f:
+            detected_objects = json.load(f)
+        # item과 일치하는 class_name을 가진 객체만 필터링
+        matching_objects = [obj for obj in detected_objects if obj['class_name'] == item]
+        return matching_objects[-1] if matching_objects else None
+    except Exception as e:
+        print(f"Error reading from JSON: {e}")
+        return None
 def search_for_object(item):
     while True:
         try:
             object_found = False
-            # 파일에서 데이터 불러오기
-            with open('detected_objects.json', 'r') as f:
-                detected_object = json.load(f)  # 딕셔너리 로드
 
-            # 탐지된 객체가 원하는 객체인지 확인
-            if detected_object['class_name'] == item:
-                print("Object detected!")
+            # item과 일치하는 class_name을 가진 객체만 필터링
+            detected_object = get_last_object(item)
+
+            if detected_object:
+                # 마지막 객체 가져오기
+                print("object detected")
                 x_coordinate = detected_object['center_x']  # 객체의 x 좌표
+                print(x_coordinate)
                 if 260 <= x_coordinate <= 400:
                     object_found = True
                     set_motor_speed(0, 0)
+                    print("Object found and within range.")
                     break  # 원하는 객체를 찾고 좌표 범위 내에 있으면 루프를 빠져나옴
                 else:
-                    set_motor_speed(-60, 60)
-                    pass
+                    set_motor_speed(-20, 20)
+                    print("Object found but not within range. Adjusting position...")
             else:
-                print("NO Object detected!")
+                print("No object found with class_name:", item)
                 # 물체를 찾지 못했으면 계속 돌기
-                set_motor_speed(-60, 60)
-                pass
+                set_motor_speed(-20, 20)
 
         except KeyboardInterrupt:
+            print("Interrupted by user.")
             break
         except Exception as e:
             print(f"Unexpected error: {e}")
+            break
+
+
+def move_to_target(item):
+    print("move_to_target")
+    while True:
+        try:
+            detected_object = get_last_object(item)
+            if detected_object:
+                distance = detected_object['distance_y']  # 객체의 y 거리
+                print(distance)
+                if distance >= 25:
+                    set_motor_speed(30, 30)
+                    print("30, 30")
+                elif 20 <= distance < 25:
+                    set_motor_speed(20, 20)
+                    print("20, 20")
+                elif 17 <= distance < 20:
+                    set_motor_speed(0, 0)
+                    print("0, 0")
+
+                    break
+                else:
+                    set_motor_speed(0, 0)
+                    print("pause")
+            else:
+                print("Object not found.")
+                search_for_object(item)
+
+            time.sleep(0.5)
+        except KeyboardInterrupt:
+            pass
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+
+def align_with_object(item):
+    while True:
+        print("align_with_object")
+        try:
+            detected_object = get_last_object(item)  # 루프 내에서 객체 정보 갱신
+            if detected_object:
+                print("align_with_object")
+                x_coordinate = detected_object['center_x']  # 객체의 x 좌표
+                print(x_coordinate)
+                if 280 <= x_coordinate <= 360:
+                    set_motor_speed(0, 0)
+                    print("Object aligned.")
+                    break
+                elif x_coordinate > 360:
+                    set_motor_speed(20, -20)
+                    print("Adjusting position to the left.")
+                else:
+                    set_motor_speed(-20, 20)
+                    print("Adjusting position to the right.")
+            else:
+                print("Object not found. Searching for object.")
+                search_for_object(item)  # 객체를 찾지 못했을 때 탐색
+
+            time.sleep(0.5)
+        except KeyboardInterrupt:
+            print("Interrupted by user.")
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+
 
 def inverse_kinematics(x_target, y_target, z_target):
     # 목표점까지의 거리 계산
     d = np.sqrt(x_target ** 2 + y_target ** 2 + z_target ** 2)
     d_prime = np.sqrt((y_target - 10) ** 2 + z_target ** 2)
-
+    print(d_prime)
+    time.sleep(1)
     # 가능한 위치인지 체크
     if d_prime > np.sqrt( link1_length ** 2 + link3_length ** 2 ):
         raise ValueError("Target is unreachable")
@@ -111,97 +187,13 @@ def inverse_kinematics(x_target, y_target, z_target):
 
     return theta1_deg, theta2_deg, theta3_deg, theta4_deg
 
-def move_to_target(item):
-    try:
-        print("move_to_target")
-        with open('detected_objects.json', 'r') as f:
-            detected_object = json.load(f)  # 딕셔너리 로드
-
-        # 탐지된 객체가 원하는 객체인지 확인
-        if detected_object['class_name'] == item:
-            while True:
-                distance = detected_object['distance_y']  # 객체의 y 거리
-                print("move_to_target")
-                if distance >= 25:
-                    set_motor_speed(60, 60)
-                    pass
-                elif 20 <= distance < 25:
-                    set_motor_speed(40, 40)
-                    pass
-                elif 17 <= distance < 20:
-                    set_motor_speed(0, 0)
-                    break
-                else:
-                    set_motor_speed(-40, -40)
-                    pass
-        else:
-            search_for_object(item)
-            # set_motor_speed(0, 0)
-            pass
-
-    except KeyboardInterrupt:
-        pass  # 중단 시 아무것도 하지 않음
-    except Exception as e:
-        print(f"Unexpected error: {e}")
-
-def align_with_object(detected_object):
-
-    # if detected_object == 'giraffe':
-    #     item = 'giraffe_head'
-    # elif detected_object == 'elephant_doll':
-    #     item = 'elephant_head'
-    # else:
-    #     pass
-
-    while True:
-        try:
-            print("align_with_object")
-            object_found = False
-            # 파일에서 데이터 불러오기
-            with open('detected_objects.json', 'r') as f:
-                detected_object = json.load(f)  # 딕셔너리 로드
-
-            if detected_object['class_name'] == item:
-                while True:
-                        print("align_with_object")
-                        x_coordinate = detected_object['center_x']  # 객체의 x 좌표
-                        if 280 <= x_coordinate <= 360:
-                            object_found = True
-                            set_motor_speed(0, 0)
-                            break  # 원하는 객체를 찾고 좌표 범위 내에 있으면 루프를 빠져나옴
-                        elif x_coordinate > 360:
-                            set_motor_speed(-40, 40)
-                            pass
-                        else:
-                            set_motor_speed(40, -40)
-                            pass
-
-            else:
-                search_for_object(item)
-                # set_motor_speed(-10, 10)
-
-        except KeyboardInterrupt:
-            break
-        except Exception as e:
-            print(f"Unexpected error: {e}")
 
 
-def grip_coordinate(items):
-    # 객체 이름 매핑
-    if items == 'giraffe':
-        item = 'giraffe_head'
-    elif items == 'elephant_doll':
-        item = 'elephant_head'
-    else:
-        item = items  # 기본값으로 items 사용
-
+def grip_coordinate(item):
     try:
         print("grip_coordinate!!")
-        with open('detected_objects.json', 'r') as f:
-            detected_object = json.load(f)  # 딕셔너리 로드
-
-        # 탐지된 객체가 원하는 객체인지 확인
-        if detected_object['class_name'] == item:
+        detected_object = get_last_object(item)
+        if detected_object:
             y_target = detected_object['distance_y']
             z_target = detected_object['z_target']
             return y_target, z_target
@@ -211,6 +203,7 @@ def grip_coordinate(items):
     except Exception as e:
         print(f"Error reading from JSON: {e}")
         return None, None
+
 
 def pick_up_object(theta2_deg, theta3_deg, theta4_deg):
 
@@ -262,17 +255,20 @@ def place_object(robot, theta2_deg, theta3_deg, theta4_deg):
 link1_length = 12.0
 link3_length = 12.0
 link4_length = 10.0
+file_path = 'cart_items.txt'
+cart_item = None  # 초기화 추가
+
+print("0")
+
 
 while True:
     try:
-        file_path = 'cart_items.txt'
         first_word = get_first_word_from_file(file_path)
-
         if first_word:
             #print("First word in the file:", first_word)
             cart_item = first_word
             print(f"you have chosen : {cart_item}")
-            time.sleep(3)
+            time.sleep(1)
 
         if cart_item == 'elephant':
             item = 'elephant_doll'
@@ -280,43 +276,55 @@ while True:
             item = 'monkey_doll'
         else:
             item = cart_item
+        print("1")
+
+        time.sleep(1)
 
         search_for_object(item)
+        time.sleep(1)
+        print("2")
         move_to_target(item)
-        # # 물체 정중앙 위치시키기
+        time.sleep(1)
+        # 물체 정중앙 위치시키기
         align_with_object(item)
-
+        time.sleep(1)
         # 좌푯값 가져와서 잡기
         x_target = 0
         y_target, z_target = grip_coordinate(item)
-        theta2_deg, theta3_deg, theta4_deg = inverse_kinematics(x_target, y_target, z_target)
+
+        print(y_target, " and ", z_target)
+        time.sleep(1)
+        theta1_deg, theta2_deg, theta3_deg, theta4_deg = inverse_kinematics(x_target, y_target, z_target)
+        print(theta1_deg, ", ", theta2_deg, ", ", theta3_deg, ", ", theta4_deg)
+        time.sleep(1)
         pick_up_object(theta2_deg, theta3_deg, theta4_deg)
-
-        item = 'target_item'
-
-        # 물체의 좌표를 가져와서 로봇 암을 해당 위치로 이동
-        search_for_object(item)
-        move_to_target(item)
-        # 좌푯값 가져와서 내려 놓기
-        align_with_object(item)
-        y_target, z_target = grip_coordinate(item)
-        theta2_deg, theta3_deg, theta4_deg = inverse_kinematics(x_target, y_target, z_target)
-
-        # 설정된 위치에 물체를 내려놓기
-        place_object(theta2_deg, theta3_deg, theta4_deg)
+        #
+        # item = 'target_item'
+        #
+        # # 물체의 좌표를 가져와서 로봇 암을 해당 위치로 이동
+        # search_for_object(item)
+        # move_to_target(item)
+        # # 좌푯값 가져와서 내려 놓기
+        # align_with_object(item)
+        # y_target, z_target = grip_coordinate(item)
+        # theta2_deg, theta3_deg, theta4_deg = inverse_kinematics(x_target, y_target, z_target)
+        #
+        # # 설정된 위치에 물체를 내려놓기
+        # place_object(theta2_deg, theta3_deg, theta4_deg)
 
         #마지막 카트 에 아이템 지우기
-        with open('cart_items.txt', 'r') as file:
-            lines = file.readlines()
+        # 마지막 카트에 아이템 지우기
+        # with open('cart_items.txt', 'r+') as file:
+        #     lines = file.readlines()
+        #     file.seek(0)  # 파일 포인터를 시작 지점으로 이동
+        #     file.truncate()  # 파일 내용을 현재 위치에서 잘라냄
+        #
+        #     # 'item'을 제외한 나머지 줄들을 유지
+        #     lines = [line for line in lines if line.strip() != item]
+        #
+        #     # 파일 다시 쓰기
+        #     file.writelines(lines)
 
-        # 'item'을 제외한 나머지 줄들을 유지
-        lines = [line for line in lines if line.strip() != item]
-        if lines:
-            lines[0] = lines[0].strip()
-
-        # 파일 다시 쓰기
-        with open('cart_items.txt', 'w') as file:
-            file.writelines(lines)
 
     except KeyboardInterrupt:
         break
